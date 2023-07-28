7fe584dd9f306cdc35f1d6a5c586bc05cb10f707
Curtis Rogers <ctrogers87@gmail.com>
2023-07-27 22:09:24 -0500

    Trying to update data... I think there is an error

--- continuous_stock.py
+++ continuous_stock.py
@@ -44,57 +44,30 @@ def init_stock_csv_file(file_path):
     df_empty.to_csv(file_path, index=False)
 
 
-# Writes new stock info to CSV
 async def update_csv_stock():
+    """Update the CSV file with the latest stock prices."""
     logger.info("Calling update_csv_stock")
+
     try:
-        companies = [
-            "Tesla Inc",
-            "General Motors Company",
-            "Toyota Motor Corporation",
-            "Ford Motor Company",
-            "Honda Motor Co",
-        ]
-        update_interval = 60  # Update every 1 minute (60 seconds)
-        total_runtime = 15 * 60  # Total runtime maximum of 15 minutes
-        num_updates = 10  # Keep the most recent 10 readings
-        logger.info(f"update_interval: {update_interval}")
-        logger.info(f"total_runtime: {total_runtime}")
-        logger.info(f"num_updates: {num_updates}")
+        # Add column headers when creating the empty CSV file for stock prices
+        file_path = Path(__file__).parent.joinpath("data").joinpath("mtcars_stock.csv")
+        if not os.path.exists(file_path):
+            df_empty = pd.DataFrame(
+                columns=["Company", "Ticker", "Time", "Price"]
+            ).copy()
+            df_empty.to_csv(file_path, index=False)
 
-        # Use a deque to store just the last, most recent 10 readings in order
-        records_deque = deque(maxlen=num_updates)
+        # Stub: Create a simple DataFrame with static data
+        df_data = pd.DataFrame({
+            "Company": ["Tesla Inc", "General Motors Company"],
+            "Ticker": ["TSLA", "GM"],
+            "Time": ["2023-07-25 12:00:00", "2023-07-25 12:01:00"],
+            "Price": [700.0, 60.0]
+        })
 
-        fp = Path(__file__).parent.joinpath("data").joinpath("mtcars_stock.csv")
-
-        # Check if the file exists, if not, create it with only the column headings
-        if not os.path.exists(fp):
-            init_stock_csv_file(fp)
-
-        logger.info(f"Initialized csv file at {fp}")
-
-        for _ in range(num_updates):  # To get num_updates readings
-            for company in companies:
-                ticker = lookup_ticker(company)
-                new_price = await get_stock_price(ticker)
-                time_now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")  # Current time
-                new_record = {
-                    "Company": company,
-                    "Ticker": ticker,
-                    "Time": time_now,
-                    "Price": new_price,
-                }
-                records_deque.append(new_record)
-
-            # Use the deque to make a DataFrame
-            df = pd.DataFrame(records_deque)
-
-            # Save the DataFrame to the CSV file, deleting its contents before writing
-            df.to_csv(fp, index=False, mode="w")
-            logger.info(f"Saving prices to {fp}")
-
-            # Wait for update_interval seconds before the next reading
-            await asyncio.sleep(update_interval)
+        # Save stock prices to the CSV file
+        logger.info(f"Saving stock prices to {file_path}")
+        df_data.to_csv(file_path, index=False)
 
     except Exception as e:
-        logger.error(f"ERROR in update_csv_stock: {e}")
+        logger.error(f"An error occurred in update_csv_stock: {e}")
