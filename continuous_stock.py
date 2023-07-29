fa9f4c182a80760b758fded5ec0ca0f40c02566b
Curtis Rogers <ctrogers87@gmail.com>
2023-07-29 02:54:08 -0500

    Attempting Stock Commit

--- continuous_stock.py
+++ continuous_stock.py
@@ -1,42 +1,47 @@
 import asyncio
 import os
+from pathlib import Path
+from datetime import datetime
+from random import randint
+
+# Import external packages
 import pandas as pd
 import yfinance as yf
 from collections import deque
-from datetime import datetime
-from random import randint
-from dotenv import load_dotenv
-from pathlib import Path
+
+# Local imports
+from fetch import fetch_from_url
 from util_logger import setup_logger
 
+# Set up logger
 logger, log_filename = setup_logger(__file__)
 
 
-def get_API_key():
-    # Keep secrets in a .env file - load it, read the values.
-    # Load environment variables from .env file
-    load_dotenv()
-    key = os.getenv("OPEN_STOCK_API_KEY")
-    return key
-
-
+# Takes company name string and returns stock ticker string
 def lookup_ticker(company):
-   stocks_dictionary = {
+    stocks_dictionary = {
         "Tesla Inc": "TSLA",
         "General Motors Company": "GM",
         "Toyota Motor Corporation": "TM",
         "Ford Motor Company": "F",
         "Honda Motor Co": "HMC",
-    } 
-   ticker = stocks_dictionary(company)
-   return ticker
-   
-async def get_stock_price(ticker):
-    logger.info(f"Calling get_stock_price for {ticker}")
-    # stock = yf.Ticker(ticker)
-    # price = stock.info["currentPrice"]
-    price = randint(132, 148)
+    }
+    ticker = stocks_dictionary[company]
+    return ticker
+
+
+# Takes ticker string and returns current stock price (asynchronous)
+async def get_stock_price(ticker: str):
+    logger.info("Calling get_stock_price for {ticker}")
+    stock_url = f"https://query1.finance.yahoo.com/v7/finance/options/{ticker}"
+    logger.info(f"Calling fetch_from_url for {stock_url}")
+    result = await fetch_from_url(stock_url)
+    logger.info(f"Data for {ticker}: {result.data}")
+    stock = yf.Ticker(ticker)
+    price = result.data["optionChain"]["result"][0]["quote"]["regularMarketPrice"]
+    price = randint()
     return price
+
 
 # Create or overwrite CSV with column headings
 def init_stock_csv_file(file_path):
@@ -44,30 +49,57 @@ def init_stock_csv_file(file_path):
     df_empty.to_csv(file_path, index=False)
 
 
+# Writes new stock info to CSV
 async def update_csv_stock():
-    """Update the CSV file with the latest stock prices."""
     logger.info("Calling update_csv_stock")
-
     try:
-        # Add column headers when creating the empty CSV file for stock prices
-        file_path = Path(__file__).parent.joinpath("data").joinpath("mtcars_stock.csv")
-        if not os.path.exists(file_path):
-            df_empty = pd.DataFrame(
-                columns=["Company", "Ticker", "Time", "Price"]
-            ).copy()
-            df_empty.to_csv(file_path, index=False)
+        companies = [
+            "Tesla Inc",
+            "General Motors Company",
+            "Toyota Motor Corporation",
+            "Ford Motor Company",
+            "Honda Motor Co",
+        ]
+        update_interval = 60  # Update every 1 minute (60 seconds)
+        total_runtime = 15 * 60  # Total runtime maximum of 15 minutes
+        num_updates = 10  # Keep the most recent 10 readings
+        logger.info(f"update_interval: {update_interval}")
+        logger.info(f"total_runtime: {total_runtime}")
+        logger.info(f"num_updates: {num_updates}")
 
-        # Stub: Create a simple DataFrame with static data
-        df_data = pd.DataFrame({
-            "Company": ["Tesla Inc", "General Motors Company"],
-            "Ticker": ["TSLA", "GM"],
-            "Time": ["2023-07-25 12:00:00", "2023-07-25 12:01:00"],
-            "Price": [700.0, 60.0]
-        })
+        # Use a deque to store just the last, most recent 10 readings in order
+        records_deque = deque(maxlen=num_updates)
 
-        # Save stock prices to the CSV file
-        logger.info(f"Saving stock prices to {file_path}")
-        df_data.to_csv(file_path, index=False)
+        fp = Path(__file__).parent.joinpath("data").joinpath("mtcars_stock.csv")
+
+        # Check if the file exists, if not, create it with only the column headings
+        if not os.path.exists(fp):
+            init_stock_csv_file(fp)
+
+        logger.info(f"Initialized csv file at {fp}")
+
+        for _ in range(num_updates):  # To get num_updates readings
+            for company in companies:
+                ticker = lookup_ticker(company)
+                new_price = await get_stock_price(ticker)
+                time_now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")  # Current time
+                new_record = {
+                    "Company": company,
+                    "Ticker": ticker,
+                    "Time": time_now,
+                    "Price": new_price,
+                }
+                records_deque.append(new_record)
+
+            # Use the deque to make a DataFrame
+            df = pd.DataFrame(records_deque)
+
+            # Save the DataFrame to the CSV file, deleting its contents before writing
+            df.to_csv(fp, index=False, mode="w")
+            logger.info(f"Saving prices to {fp}")
+
+            # Wait for update_interval seconds before the next reading
+            await asyncio.sleep(update_interval)
 
     except Exception as e:
-        logger.error(f"An error occurred in update_csv_stock: {e}")
+        logger.error(f"ERROR in update_csv_stock: {e}")
